d637fef93ab9b96bfd32b199d28abf7c3ff9dd91
LORENZO MANNINO s334210 <s334210@studenti.polito.it>
2024-11-04 20:29:08 +0100

    add 7 documents to populate.mjs

--- server/src/sequelize/populate.mjs
+++ server/src/sequelize/populate.mjs
@@ -67,6 +67,78 @@ export default async function populateDB(sequelize) {
                     description:
                         "The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project.",
                 },
+                {
+                    title: "Deformation forecast (45)",
+                    scaleType: "Plan",
+                    scaleValue: "1:12.500",
+                    issuanceDate: "2014-12",
+                    type: "Technical Document",
+                    language: "Swedish",
+                    pages: "1",
+                    description:
+                        "The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project"
+                },
+
+                {
+                    title: "Adjusted development plan (47)",
+                    scaleType: "Scale",
+                    scaleValue: "1:7.500",
+                    issuanceDate: "2015",
+                    type: "Design Document",
+                    language: "Swedish",
+                    pages: "1",
+                    description: "This document is the update of the Development Plan, one year after its creation, modifications are made to the general master plan, which is published under the name 'Adjusted Development Plan91,' and still represents the version used today after 10 years. Certainly, there are no drastic differences compared to the previous plan, but upon careful comparison, several modified elements stand out. For example, the central square now takes its final shape, as well as the large school complex just north of it, which appears for the first time."
+                },
+                {
+                    title: "Detail plan for square and commercial street (50)",
+                    scaleType: "Scale",
+                    scaleValue: "1:1.000",
+                    issuanceDate: "2016-06-22",
+                    type: "Prescriptive Document",
+                    language: "Swedish",
+                    pages: "1-43",
+                    description: "This plan, approved in July 2016, is the first detailed plan to be implemented from the new masterplan (Adjusted development plan). The document defines the entire area near the town hall, comprising a total of 9 blocks known for their density. Among these are the 6 buildings that will face the main square. The functions are mixed, both public and private, with residential being prominent, as well as the possibility of incorporating accommodation facilities such as hotels. For all buildings in this plan, the only height limit is imposed by air traffic."
+                },
+                {
+                    title: "Construction of Scandic Hotel begins (63)",
+                    scaleType: "Blueprints/effects",
+                    issuanceDate: "2019-04",
+                    type: "Material Effect",
+                    language: "-",
+                    pages: "-",
+                    coordinates: "67°50'54.7\"N 20°18'17.2\"E",
+                    description: "After two extensions of the land acquisition agreement, necessary because this document in Sweden is valid for only two years, construction of the hotel finally began in 2019."
+                },
+                {
+                    title: "Town Hall demolition (64)",
+                    scaleType: "Blueprints/effects",
+                    issuanceDate: "2019-04",
+                    type: "Material Effect",
+                    language: "-",
+                    pages: "-",
+                    coordinates: "67°51'09.0\"N 20°13'20.8\"E",
+                    description: "After the construction of the new town hall was completed, the old building, nicknamed \"The Igloo,\" was demolished. The only elements preserved were the door handles, a masterpiece of Sami art made of wood and bone, and the clock tower, which once stood on the roof of the old town hall. The clock tower was relocated to the central square of New Kiruna, in front of the new building."
+                },
+                {
+                    title: "Construction of Aurora Center begins (65)",
+                    scaleType: "Blueprints/effects",
+                    issuanceDate: "2019-05",
+                    type: "Material Effect",
+                    language: "-",
+                    pages: "-",
+                    coordinates: "67°50'57.0\"N 20°18'15.8\"E",
+                    description: "Shortly after the construction of the Scandic hotel began, work on the Aurora Center also started, a multifunctional complex that includes the municipal library of Kiruna. The two buildings are close to each other and connected by a skywalk, just like in the old town center."
+                },
+                {
+                    title: "Construction of Block 1 begins(69)",
+                    scaleType: "Blueprints/effects",
+                    issuanceDate: "2019-06",
+                    type: "Material Effect",
+                    language: "-",
+                    pages: "-",
+                    coordinates: "67°50'54.8\"N 20°18'01.2\"E",
+                    description: "Simultaneously with the start of construction on the Aurora Center, work also began on Block 1, another mixed-use building overlooking the main square and the road leading to old Kiruna. These are the first residential buildings in the new town."
+                }
             ],
             { validate: true }
         );
@@ -77,6 +149,15 @@ export default async function populateDB(sequelize) {
         ]);
         await documents[1].setStakeholders(stakeholders[1]);
         await documents[2].setStakeholders([stakeholders[1], stakeholders[3]]);
+
+        await documents[3].setStakeholders(stakeholders[0]);
+        await documents[4].setStakeholders([stakeholders[1], stakeholders[3]]);
+        await documents[5].setStakeholders(stakeholders[1]);
+        await documents[6].setStakeholders(stakeholders[0]);
+        await documents[7].setStakeholders(stakeholders[0]);
+        await documents[8].setStakeholders(stakeholders[0]);
+        await documents[9].setStakeholders(stakeholders[0]);
+
 
         await documents[0].addConnectedDocument(documents[1], {
             through: { relationship: "Update" },
