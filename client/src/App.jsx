579bf0b1803679d74d0873079af20e81506990c5
Luca Ferraris <ferraris.luca2001@gmail.com>
2024-11-06 10:06:43 +0100

    Add two components Header and Footer for have a more clear App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import DescriptionForm from "./Components/Form";
 import HomePage from "./Components/HomePage";
 import LoginForm from "./Components/LoginForm";
 import RegistrationForm from "./Components/RegistrationForm";
+import Header from "./components/Header";
+import Footer from "./Components/Footer";
 import API from "./API/API.mjs";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -41,29 +43,8 @@ function App() {
     return (
         <div style={{ position: "relative", height: "100vh", paddingTop: contentPadding }}>
             {/* Header */}
-            <header
-                className={`d-flex justify-content-between align-items-center p-3 w-100 ${headerClass}`}
-                style={{ position: "fixed", top: 0, zIndex: 2, height: "60px" }}
-            >
-                <h1 className="m-0">
-                    <Link to="/" className={isHomePage ? "text-dark text-decoration-none" : "text-dark text-decoration-none"}>
-                        Kiruna eXplorer
-                    </Link>
-                </h1>
-                <div>
-                    {isLoggedIn ? (
-                        <button className="btn btn-dark" onClick={handleLogout}>
-                            Logout
-                        </button>
-                    ) : (
-                        location.pathname !== "/login" && (
-                            <Link to="/login" className="btn btn-dark">
-                                Login
-                            </Link>
-                        )
-                    )}
-                </div>
-            </header>
+
+            <Header isLoggedIn={isLoggedIn} handleLogout={handleLogout} headerClass={headerClass} isHomePage={isHomePage} />
 
             {/* Routes */}
             <HomePage />
@@ -72,75 +53,9 @@ function App() {
                 <Route path="/login" element={<LoginForm handleLogin={handleLogin} />} />
                 <Route path="/registration" element={<RegistrationForm handleLogin={handleLogin} />} />
             </Routes>
+
             {/* Buttons for the home page */}
-            {isHomePage && isLoggedIn && (
-                <div className="position-fixed d-flex flex-column gap-1 bottom-0 end-0 mb-4 me-1">
-                    <Link
-                        to="/add"
-                        className="btn d-flex align-items-center justify-content-center"
-                        style={{
-                            width: "4rem",
-                            height: "4rem",
-                            backgroundColor: "#000000",
-                            borderRadius: "50%",
-                            color: "white",
-                            transition: "font-size 0.3s, width 0.3s, border-radius 0.3s",
-                            position: "relative",
-                        }}
-                        title="Add Document"
-                        onMouseEnter={(e) => {
-                            e.currentTarget.style.width = "8rem";
-                            e.currentTarget.style.borderRadius = "0.5rem";
-                            e.currentTarget.querySelector(".add-text").style.transition = "opacity 0.3s 0.1s";
-                            e.currentTarget.querySelector(".add-text").style.opacity = "1";
-                            e.currentTarget.querySelector(".add-icon").style.opacity = "0";
-                        }}
-                        onMouseLeave={(e) => {
-                            e.currentTarget.style.width = "4rem";
-                            e.currentTarget.style.borderRadius = "50%";
-                            e.currentTarget.querySelector(".add-text").style.transition = "none";
-                            e.currentTarget.querySelector(".add-text").style.opacity = "0";
-                            e.currentTarget.querySelector(".add-icon").style.opacity = "1";
-                        }}
-                    >
-                        <span className="add-icon" style={{ fontSize: "2rem" }}>
-                            <i className="bi bi-plus"></i>
-                        </span>
-                        <span
-                            style={{
-                                position: "absolute",
-                                opacity: "0",
-                                fontSize: "1rem",
-                            }}
-                            className="add-text"
-                        >
-                            Add Document
-                        </span>
-                    </Link>
-                    {/*<Link to="/connect" className="btn btn-dark custom-link">
-            Connect Documents
-        </Link> */}
-                </div>
-            )}
-            {location.pathname === "/add" && (
-                <div className="position-fixed d-flex flex-column gap-1 bottom-0 end-0 mb-4 me-1">
-                    <Link
-                        to="/"
-                        className="btn d-flex align-items-center justify-content-center"
-                        style={{
-                            width: "4rem",
-                            height: "4rem",
-                            backgroundColor: "red",
-                            borderRadius: "50%",
-                            color: "white",
-                            transition: "font-size 0.3s, width 0.3s, border-radius 0.3s",
-                            position: "relative",
-                        }}
-                    >
-                        <i className="bi bi-x" style={{ transition: "opacity 0.3s", fontSize: "2rem" }}></i>
-                    </Link>
-                </div>
-            )}
+            <Footer isHomePage={isHomePage} isLoggedIn={isLoggedIn} location={location} />
         </div>
     );
 }
