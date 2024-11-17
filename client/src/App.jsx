0211009ce47bce804ff60920912934da19510fbd
Cremona Antoine <101284842+Entwaan@users.noreply.github.com>
2024-11-17 15:30:35 +0100

    fix import in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import DescriptionForm from "./Components/Form";
 import HomePage from "./Components/HomePage";
 import LoginForm from "./Components/LoginForm";
 import RegistrationForm from "./Components/RegistrationForm";
-import Header from "./components/Header";
+import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import API from "./API/API.mjs";
 import "bootstrap-icons/font/bootstrap-icons.css";
