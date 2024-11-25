e92f5a48639ca7e280a28facde8d27015775acaa
Cremona Antoine <101284842+Entwaan@users.noreply.github.com>
2024-11-25 18:59:42 +0100

    improving fix of findOriginalResources

--- server/src/utils/manageResources.mjs
+++ server/src/utils/manageResources.mjs
@@ -1,50 +1,26 @@
 import fs from "fs";
 import path from "path";
 
-// export async function findOriginalResources(id) {
-//     const folderPath = path.join("document_resources", id.toString(), "original_resources");
-
-//     try {
-//         const files = await fs.promises.readdir(folderPath);
-//         return files;
-//     } catch (err) {
-//         if (err.code === "ENOENT") {
-//             // Folder does not exist
-//             return [];
-//         } else {
-//             // Other errors
-//             throw err;
-//         }
-//     }
-// }
-function isValidId(id) {
-    return /^[a-zA-Z0-9_-]+$/.test(id);
-}
-
 export async function findOriginalResources(id) {
-    if (!isValidId(id.toString())) {
-        throw new Error("Invalid ID");
-    }
-
-    const baseFolderPath = path.resolve("document_resources");
-    const folderPath = path.join(baseFolderPath, id.toString(), "original_resources");
-
-    if (!folderPath.startsWith(baseFolderPath)) {
-        throw new Error("Path traversal attempt detected");
-    }
-
     try {
+        const createdPath = path.join("document_resources", id.toString(), "original_resources");
+        const folderPath = await fs.promises.realpath(createdPath);
+        if (!folderPath.startsWith(path.join(process.cwd(), "document_resources"))) {
+            throw new Error("Invalid path");
+        }
         const files = await fs.promises.readdir(folderPath);
         return files;
     } catch (err) {
         if (err.code === "ENOENT") {
-            // non existing
+            // Folder does not exist
             return [];
         } else {
+            // Other errors
             throw err;
         }
     }
 }
+
 export function deleteOriginalResources(id) {
     const folderPath = path.join("document_resources", id.toString(), "original_resources");
 
