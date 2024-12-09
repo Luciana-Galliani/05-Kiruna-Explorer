a4ced0400b929e7bbf3cadce2b4c8bff37feecf3
Luca Ferraris <ferraris.luca2001@gmail.com>
2024-12-09 21:59:58 +0100

    Insert the new icons in the utils

---
+++ client/src/Components/reactIcons/conflictIcon.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+const ConflitctIcon = ({
+  size = 24,
+  fillPrimary = "white",
+  fillSecondary = "gray",
+  ...props
+}) => (
+   <svg
+      fill="currentColor"
+      version="1.1"
+      id="Capa_1"
+      width="80"
+      height="80"
+      viewBox="0 0 468.613 468.614"
+      xml:space="preserve"
+      sodipodi:docname="conflict.svg"
+      inkscape:version="1.4 (86a8ad7, 2024-10-11)"
+      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:svg="http://www.w3.org/2000/svg"><defs
+      id="defs4" /><sodipodi:namedview
+      id="namedview4"
+      pagecolor="#ffffff"
+      bordercolor="#000000"
+      borderopacity="0.25"
+      inkscape:showpageshadow="2"
+      inkscape:pageopacity="0.0"
+      inkscape:pagecheckerboard="0"
+      inkscape:deskcolor="#d1d1d1"
+      showguides="true"
+      inkscape:zoom="9.6625"
+      inkscape:cx="40"
+      inkscape:cy="40"
+      inkscape:window-width="1920"
+      inkscape:window-height="991"
+      inkscape:window-x="-9"
+      inkscape:window-y="-9"
+      inkscape:window-maximized="1"
+      inkscape:current-layer="Capa_1" />
+   <circle
+      cx="234.3065"
+      cy="234.30701"
+      fill="#d3d3d3"
+      id="circle1"
+      style={{strokeWidth:1, fill:"#000000"}}
+      r="50.000053%" /><circle
+      style={{fill:"#ffffff", strokeWidth:41.9546}}
+      id="path4"
+      cx="234.3065"
+      cy="234.30701"
+      r="205.01863"
+      fill={{fillPrimary}} /><g
+      id="g3"
+      transform="matrix(0.62558343,0,0,0.8276804,270.38278,34.324288)">
+   	<g
+      transform="matrix(0.9,0,0,0.6,-756,51.4)"
+      id="g2-9">
+   		<path
+      d="m 1029.9,256.6 h -3.5 c 10.6,-12.4 17.1,-28.4 17.1,-45.9 0,-38.9 -31.7,-70.6 -70.6,-70.6 -38.9,0 -70.6,31.7 -70.6,70.6 0,17.5 6.5,33.6 17.1,45.9 h -3.5 c -19.7,0 -35.6,16 -35.6,35.6 v 117.1 c 0,19.6 16,35.6 35.6,35.6 h 1.4 V 573 c 0,20.3 10.7,35.6 24.9,35.6 h 61.5 c 14.2,0 24.9,-15.3 24.9,-35.6 V 445 h 1.4 c 19.6,0 35.6,-16 35.6,-35.6 V 292.3 c 0,-19.7 -16,-35.7 -35.7,-35.7 z"
+      id="path1-4" />
+   	</g>
+   </g>
+   <g
+      id="g2"
+      transform="matrix(0.62558343,0,0,0.8276804,47.601689,34.324288)">
+   	<g
+      transform="matrix(0.9,0,0,0.6,-756,51.4)"
+      id="g1">
+   		<path
+      d="m 1029.9,256.6 h -3.5 c 10.6,-12.4 17.1,-28.4 17.1,-45.9 0,-38.9 -31.7,-70.6 -70.6,-70.6 -38.9,0 -70.6,31.7 -70.6,70.6 0,17.5 6.5,33.6 17.1,45.9 h -3.5 c -19.7,0 -35.6,16 -35.6,35.6 v 117.1 c 0,19.6 16,35.6 35.6,35.6 h 1.4 V 573 c 0,20.3 10.7,35.6 24.9,35.6 h 61.5 c 14.2,0 24.9,-15.3 24.9,-35.6 V 445 h 1.4 c 19.6,0 35.6,-16 35.6,-35.6 V 292.3 c 0,-19.7 -16,-35.7 -35.7,-35.7 z"
+      id="path1" />
+   	</g>
+   </g>
+
+   <g
+      id="g4"
+      transform="matrix(1.1342461,0,0,1.1342461,-47.605394,-8.5211695)">
+   	<text
+      transform="matrix(3.6,0,0,2.7,-87,-17.5)"
+      x="19.9%"
+      y="9.6999998%"
+      text-anchor="middle"
+      fill="currentColor"
+      font-size="48px"
+      font-family="Arial"
+      dy="0.30000001em"
+      id="text3">X</text>
+   </g>
+   </svg>
+    
+);
+
+export default ConflitctIcon
