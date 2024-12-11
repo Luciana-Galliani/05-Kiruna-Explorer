159348c22c936c98cdacbc49edbc32bd7d515865
Cremona Antoine <101284842+Entwaan@users.noreply.github.com>
2024-12-11 19:49:09 +0100

    add connections types on diagram (with curve) + title hover + "Plan" title

--- client/src/Components/Diagram.jsx
+++ client/src/Components/Diagram.jsx
@@ -2,15 +2,28 @@ import React, { useState, useEffect, useRef } from "react";
 import API from "../API/API.mjs";
 import * as d3 from "d3";
 import { min, max, range } from "d3-array";
+import { use } from "react";
+
+// Constants
+const NODE_RADIUS = 10;
+const LINE = d3.line().curve(d3.curveBasis); // Bezier curve for connections
+const MARGIN = { top: 50, right: 50, bottom: 60, left: 200 };
+const RELATIONSHIP_STYLES = {
+    "Direct Consequence": "0", // Solid line
+    "Collateral Consequence": "8, 5", // Dashed line
+    Prevision: "2, 2", // Dotted line
+    Update: "2, 5, 10, 5", // Dash-dotted line
+};
 
 // Function to process the documents and return the nodes and links
 const processDocuments = (documents) => {
     const scales = new Map();
     const nodes = [];
     const links = [];
-    const minYear = min(documents.map((doc) => new Date(doc.issuanceDate).getFullYear()));
-    const maxYear = max(documents.map((doc) => new Date(doc.issuanceDate).getFullYear()));
-    const years = range(minYear, maxYear + 1);
+    const seenConnections = new Set(); // Set to store unique connections
+    const minYear = Math.min(...documents.map((doc) => new Date(doc.issuanceDate).getFullYear()));
+    const maxYear = Math.max(...documents.map((doc) => new Date(doc.issuanceDate).getFullYear()));
+    const years = Array.from({ length: maxYear - minYear + 1 }, (_, i) => minYear + i);
 
     documents.forEach((doc) => {
         const issuanceDate = new Date(doc.issuanceDate);
@@ -38,11 +51,18 @@ const processDocuments = (documents) => {
 
         // Add connections
         doc.connections.forEach((connection) => {
-            links.push({
-                source: doc.id,
-                target: connection.targetDocument.id,
-                relationship: connection.relationship,
-            });
+            const source = doc.id;
+            const target = connection.targetDocument.id;
+            const relationship = connection.relationship;
+
+            // Create a unique key for the connection
+            const connectionKey = [source, target].sort().join("-") + "-" + relationship;
+
+            // Add connection only if it's not already seen
+            if (!seenConnections.has(connectionKey)) {
+                seenConnections.add(connectionKey);
+                links.push({ source, target, relationship });
+            }
         });
     });
 
@@ -51,7 +71,6 @@ const processDocuments = (documents) => {
 
 export default function Diagram() {
     const [documents, setDocuments] = useState([]);
-    const [loading, setLoading] = useState(true);
     const svgRef = useRef();
     const [dimensions, setDimensions] = useState({
         width: window.innerWidth,
@@ -72,7 +91,6 @@ export default function Diagram() {
             try {
                 const response = await API.getDocuments();
                 setDocuments(response.documents);
-                setLoading(false);
             } catch (error) {
                 console.log(error);
             }
@@ -83,19 +101,35 @@ export default function Diagram() {
     useEffect(() => {
         const { nodes, links, scales, years } = processDocuments(documents);
 
-        const margin = { top: 50, right: 50, bottom: 60, left: 110 };
-        const width = dimensions.width - margin.left - margin.right;
-        const height = dimensions.height - 60 - margin.top - margin.bottom;
+        // Main graph
+        const width = dimensions.width - MARGIN.left - MARGIN.right;
+        const height = dimensions.height - 60 - MARGIN.top - MARGIN.bottom;
 
         const svg = d3.select(svgRef.current);
 
         svg.selectAll("*").remove();
 
         const g = svg
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
+            .attr("width", width + MARGIN.left + MARGIN.right)
+            .attr("height", height + MARGIN.top + MARGIN.bottom)
             .append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+            .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`);
+
+        // Tooltip to show document title on hover (hidden by default)
+        const titleTooltip = d3
+            .select("body")
+            .append("div")
+            .attr("class", "titleTooltip")
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden")
+            .style("opacity", 1)
+            .style("display", "none")
+            .style("background-color", "rgba(0, 0, 0, 0.7)")
+            .style("color", "white")
+            .style("padding", "5px")
+            .style("border-radius", "4px")
+            .style("font-size", "12px");
 
         // Scales
         const xScale = d3
@@ -129,30 +163,63 @@ export default function Diagram() {
             .attr("y1", (d) => (yScale(d) % 1 === 0.0 ? yScale(d) + 0.5 : yScale(d))) // add 0.5 to avoid weird graphical glitches
             .attr("y2", (d) => (yScale(d) % 1 === 0.0 ? yScale(d) + 0.5 : yScale(d))) // add 0.5 to avoid weird graphical glitches
             .attr("stroke", "black")
-            .attr("stroke-width", "1");
+            .attr("stroke-width", "2");
 
         // Axis
         g.append("g").call(d3.axisLeft(yScale));
         g.append("g").call(d3.axisTop(xScale));
+        // Style for axis lines
+        g.selectAll(".domain, .tick line").attr("stroke-width", 2); // Increase axis line thickness
+        // Style for axis labels (ticks)
+        g.selectAll(".tick text").style("font-size", "14px").style("font-weight", "bold"); // Increase font size for axis labels
+
+        // Add title "Plan" in the middle of the "1:n" scales
+        const planLabels = scales.filter((d) => d.startsWith("1:"));
+        if (planLabels.length > 0) {
+            const middleY =
+                (yScale(planLabels[0]) + yScale(planLabels[planLabels.length - 1])) / 2 +
+                yScale.bandwidth() / 2;
+
+            g.append("text")
+                .attr("x", -80)
+                .attr("y", middleY)
+                .attr("text-anchor", "end")
+                .style("font-size", "14px")
+                .style("font-weight", "bold")
+                .style("font-family", "sans-serif")
+                .text("Plan");
+        }
 
         // Connections
         g.selectAll(".link")
             .data(links)
             .enter()
-            .append("line")
+            .append("path")
             .attr("class", "link")
-            .attr("x1", (d) => xScale(new Date(nodes.find((n) => n.id === d.source).date)))
-            .attr(
-                "y1",
-                (d) => yScale(nodes.find((n) => n.id === d.source).scale) + yScale.bandwidth() / 2
-            )
-            .attr("x2", (d) => xScale(new Date(nodes.find((n) => n.id === d.target).date)))
-            .attr(
-                "y2",
-                (d) => yScale(nodes.find((n) => n.id === d.target).scale) + yScale.bandwidth() / 2
-            )
+            .attr("d", (d) => {
+                const sourceNode = nodes.find((n) => n.id === d.source);
+                const targetNode = nodes.find((n) => n.id === d.target);
+
+                const sourceX = Math.max(xScale(new Date(sourceNode.date)), NODE_RADIUS);
+                const sourceY = yScale(sourceNode.scale) + yScale.bandwidth() / 2;
+                const targetX = Math.max(xScale(new Date(targetNode.date)), NODE_RADIUS);
+                const targetY = yScale(targetNode.scale) + yScale.bandwidth() / 2;
+
+                // Control point for the Bezier curve
+                const controlPointX =
+                    Math.min(sourceX, targetX) + Math.abs(sourceX - targetX) * 0.2;
+                const controlPointY = (sourceY + targetY) / 2 + Math.abs(sourceY - targetY) * 0.4;
+
+                return LINE([
+                    [sourceX, sourceY],
+                    [controlPointX, controlPointY],
+                    [targetX, targetY],
+                ]);
+            })
             .attr("stroke", "black")
-            .attr("stroke-width", 1);
+            .attr("stroke-width", 1.5)
+            .attr("stroke-dasharray", (d) => RELATIONSHIP_STYLES[d.relationship] || "0")
+            .attr("fill", "none");
 
         // Nodes
         g.selectAll(".node")
@@ -160,12 +227,49 @@ export default function Diagram() {
             .enter()
             .append("circle")
             .attr("class", "node")
-            .attr("cx", (d) => xScale(new Date(d.date)))
+            .attr("cx", (d) => max([xScale(new Date(d.date)), NODE_RADIUS]))
             .attr("cy", (d) => {
                 return yScale(d.scale) + yScale.bandwidth() / 2;
             })
-            .attr("r", 5)
-            .attr("fill", (d) => (d.stakeholders.length ? d.stakeholders[0].color : "#000"));
+            .attr("r", NODE_RADIUS)
+            .attr("fill", (d) => (d.stakeholders.length ? d.stakeholders[0].color : "#000"))
+            .on("mouseover", (event, d) => {
+                const rect = svgRef.current.getBoundingClientRect(); // Get SVG position
+                titleTooltip
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                    .text(d.title)
+                    .style(
+                        "left",
+                        `${
+                            rect.left + MARGIN.left + parseFloat(d3.select(event.target).attr("cx"))
+                        }px`
+                    )
+                    .style(
+                        "top",
+                        `${
+                            rect.top +
+                            MARGIN.top +
+                            parseFloat(d3.select(event.target).attr("cy") - NODE_RADIUS - 5)
+                        }px`
+                    )
+                    .style("transform", "translate(-50%, -100%)");
+
+                // Change border color
+                d3.select(event.target).attr("stroke", "white").attr("stroke-width", 2);
+                // Move the hovered node to the front
+                d3.select(event.target).raise();
+            })
+            .on("mouseout", (event) => {
+                titleTooltip.style("visibility", "hidden");
+                // Reset border color
+                d3.select(event.target).attr("stroke", "none");
+            });
+
+        return () => {
+            titleTooltip.remove();
+            g.selectAll("*").remove();
+        };
     }, [documents, dimensions]);
 
     return <svg ref={svgRef}></svg>;
